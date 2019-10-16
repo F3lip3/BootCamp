2f224ca9fb1f9bf501aa9cecdd194c6f4e499588
Felipe H. Teixeira <felipe@dinfo.com.br>
2019-10-16 14:58:57 -0300

    ESLint configs and sequelize structure

--- desafio02/src/routes.js
+++ desafio02/src/routes.js
@@ -3,8 +3,6 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-    let test = 'a';
-    if (test === 'a') test = 'b';
     return res.json({ message: 'Hello World' });
 });
 
