c7f1b1d9b0311cd4388d4bb84741d2b8ab8d4387
Felipe H. Teixeira <felipe@dinfo.com.br>
2019-11-10 22:31:47 -0300

    Create home page structure

--- desafio07/mobile/src/pages/Home/index.js
+++ desafio07/mobile/src/pages/Home/index.js
@@ -1,12 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FlatList } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import {
+  Container,
+  Product,
+  ProductImage,
+  ProductTitle,
+  ProductPrice,
+  AddButton,
+  ProductAmount,
+  ProductAmountText,
+  AddButtonText
+} from './styles';
 
-export default function Home() {
-  return (
-    <Container>
-      <FlatList />
-    </Container>
-  );
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
+
+class Home extends Component {
+  state = {
+    products: []
+  };
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts = async () => {
+    const response = await api.get('/products');
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price)
+    }));
+    this.setState({ products: data });
+  };
+
+  renderProduct = ({ item }) => {
+    return (
+      <Product key={item.id}>
+        <ProductImage source={{ uri: item.image }} />
+        <ProductTitle>{item.title}</ProductTitle>
+        <ProductPrice>{item.priceFormatted}</ProductPrice>
+        <AddButton>
+          <ProductAmount>
+            <Icon name="add-shopping-cart" color="#fff" size={20} />
+            <ProductAmountText>0</ProductAmountText>
+          </ProductAmount>
+          <AddButtonText>ADICIONAR</AddButtonText>
+        </AddButton>
+      </Product>
+    );
+  };
+
+  render() {
+    const { products } = this.state;
+    return (
+      <Container>
+        <FlatList
+          horizontal
+          data={products}
+          extraData={this.props}
+          keyExtractor={item => String(item.id)}
+          renderItem={this.renderProduct}
+        />
+      </Container>
+    );
+  }
 }
+
+export default Home;
