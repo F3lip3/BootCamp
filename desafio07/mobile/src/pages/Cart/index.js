72f8bf17ae5a697624fb52c4fe04224bb07c8125
Felipe H. Teixeira <felipe@dinfo.com.br>
2019-11-10 23:49:31 -0300

    Create cart structure

--- desafio07/mobile/src/pages/Cart/index.js
+++ desafio07/mobile/src/pages/Cart/index.js
@@ -1,8 +1,101 @@
 import React from 'react';
-import { Text } from 'react-native';
+import PropTypes from 'prop-types';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Products,
+  Product,
+  ProductInfo,
+  ProductImage,
+  ProductDetails,
+  ProductTitle,
+  ProductPrice,
+  ProductDelete,
+  ProductControls,
+  ProductControlButton,
+  ProductAmount,
+  ProductSubTotal,
+  TotalContainer,
+  TotalText,
+  TotalAmount,
+  Order,
+  OrderText,
+  EmptyContainer,
+  EmptyText
+} from './styles';
+import colors from '../../styles/colors';
 
-export default function Cart() {
-  return <Text>CART</Text>;
+function Cart({ products }) {
+  return (
+    <Container>
+      {products.length ? (
+        <>
+          <Products>
+            {products.map(product => (
+              <Product key={product.id}>
+                <ProductInfo>
+                  <ProductImage source={{ uri: product.image }} />
+                  <ProductDetails>
+                    <ProductTitle>{product.title}</ProductTitle>
+                    <ProductPrice>{product.priceFormatted}</ProductPrice>
+                    <ProductDelete>
+                      <Icon
+                        name="delete-forever"
+                        size={24}
+                        color={colors.primary}
+                      />
+                    </ProductDelete>
+                  </ProductDetails>
+                </ProductInfo>
+                <ProductControls>
+                  <ProductControlButton>
+                    <Icon
+                      name="remove-circle-outline"
+                      size={20}
+                      color={colors.primary}
+                    />
+                  </ProductControlButton>
+                  <ProductAmount value={0} />
+                  <ProductControlButton>
+                    <Icon
+                      name="add-circle-outline"
+                      size={20}
+                      color={colors.primary}
+                    />
+                  </ProductControlButton>
+                  <ProductSubTotal>0</ProductSubTotal>
+                </ProductControls>
+              </Product>
+            ))}
+          </Products>
+          <TotalContainer>
+            <TotalText>TOTAL</TotalText>
+            <TotalAmount>0</TotalAmount>
+            <Order>
+              <OrderText>FINALIZAR PEDIDO</OrderText>
+            </Order>
+          </TotalContainer>
+        </>
+      ) : (
+        <EmptyContainer>
+          <Icon name="remove-shopping-cart" size={64} color="#eee" />
+          <EmptyText>Seu carrinho está vazio.</EmptyText>
+        </EmptyContainer>
+      )}
+    </Container>
+  );
 }
+
+Cart.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      image: PropTypes.string,
+      title: PropTypes.string,
+      priceFormatted: PropTypes.string
+    })
+  ).isRequired
+};
+
+export default Cart;
